71b92443b462e49942e1f83b78d907642a3b9cc3
Natasha Bartley <+>
2023-03-17 09:15:48 -0400

    commit tree a275bebc1df8f722f83118ad85e492389ef3d442

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// Code your solutions in this file
+const address = {
+    street1: '11 Broadway',
+    street2: '2nd Floor',
+    city: 'New York',
+    state: 'NY',
+    zipCode: "10004"
+  };
+  
+address.key = "Let's have a 'key' key!";
+
+for (const key in address) {
+  console.log(address.key);
+}
